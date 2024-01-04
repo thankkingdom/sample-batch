5c8f115b6c693e6153347e250d75ca0d6d1e2828
thankkingdom <thankkingdom@gmail.com>
2024-01-04 10:48:34 +0900

    バージョンアップなど

--- src/test/java/com/example/samplebatch/SampleBatchApplicationTests.java
+++ src/test/java/com/example/samplebatch/SampleBatchApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.samplebatch;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SampleBatchApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
